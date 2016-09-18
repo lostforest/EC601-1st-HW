08ed9ff577eb60691acf36aff5568480a996ed3d
lostforest <yzxregist@gmail.com>
2016-09-18 12:24:00 -0700

    OpenCV files and result

---
+++ OpenCV/OpenCV-Test/CMakeFiles/edge.dir/DependInfo.cmake
@@ -0,0 +1,40 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/lostforest/test/edge.cpp" "/home/lostforest/test/CMakeFiles/edge.dir/edge.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/lostforest/OpenCV"
+  "/home/lostforest/OpenCV/opencv/include"
+  "/home/lostforest/OpenCV/opencv/include/opencv"
+  "/home/lostforest/OpenCV/opencv/modules/core/include"
+  "/home/lostforest/OpenCV/opencv/modules/flann/include"
+  "/home/lostforest/OpenCV/opencv/modules/imgproc/include"
+  "/home/lostforest/OpenCV/opencv/modules/ml/include"
+  "/home/lostforest/OpenCV/opencv/modules/photo/include"
+  "/home/lostforest/OpenCV/opencv/modules/video/include"
+  "/home/lostforest/OpenCV/opencv/modules/imgcodecs/include"
+  "/home/lostforest/OpenCV/opencv/modules/shape/include"
+  "/home/lostforest/OpenCV/opencv/modules/videoio/include"
+  "/home/lostforest/OpenCV/opencv/modules/highgui/include"
+  "/home/lostforest/OpenCV/opencv/modules/objdetect/include"
+  "/home/lostforest/OpenCV/opencv/modules/superres/include"
+  "/home/lostforest/OpenCV/opencv/modules/ts/include"
+  "/home/lostforest/OpenCV/opencv/modules/features2d/include"
+  "/home/lostforest/OpenCV/opencv/modules/calib3d/include"
+  "/home/lostforest/OpenCV/opencv/modules/stitching/include"
+  "/home/lostforest/OpenCV/opencv/modules/videostab/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
